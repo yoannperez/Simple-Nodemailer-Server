db2df60e9186533a1e8026975e46de5511cc6503
Yoann Perez <yoann.perez@gmail.com>
2022-01-21 10:44:36 +0100

    Working on http

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
+// middleware
+app.use(express.json());
+app.use(cors());
+
 let transporter = nodemailer.createTransport({
-  service: "gmail",
+  pool: true,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  secure: true, // upgrade later with STARTTLS
   auth: {
-    type: "OAuth2",
     user: process.env.EMAIL,
     pass: process.env.WORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   },
 });
 
@@ -19,19 +23,29 @@ transporter.verify((err, success) => {
   err ? console.log(err) : console.log(`=== Server is ready to take messages: ${success} ===`);
 });
 
-let mailOptions = {
-  from: "test@gmail.com",
-  to: process.env.EMAIL,
-  subject: "Nodemailer API",
-  text: "Hi from your nodemailer API",
-};
+// Route for sending mail
+app.post("/send", function (req, res) {
 
-transporter.sendMail(mailOptions, function (err, data) {
-  if (err) {
-    console.log("Error " + err);
-  } else {
-    console.log("Email sent successfully");
-  }
+  let mailOptions = {
+    from: `${req.body.mailerState.email}`,
+    to: process.env.EMAIL,
+    subject: `Message from: ${req.body.mailerState.email}`,
+    text: `${req.body.mailerState.message}`,
+    date: new Date("2000-01-01 00:00:00"),
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+    } else {
+        console.log("== Message Sent ==");
+        res.json({
+          status: "success",
+        });
+    }
+  });
 });
 
 // Start Node Express Server
